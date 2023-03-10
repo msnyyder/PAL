5e4a9cf5d851eb4a098021008c371330216bf3af
lDavis956 <116458122+lDavis956@users.noreply.github.com>
2023-03-10 13:14:32 -0600

    Creating database models

--- courseRegistration/admin.py
+++ courseRegistration/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+#display all schedules
+class ScheduleDisplay(admin.ModelAdmin):
+    list_display = ('name', 'semester', 'year', 'courses')
+    search_fields = ('name', 'semster', 'year')
+admin.site.register(Schedule, ScheduleDisplay)
+
+class RealScheduleDisplay(admin.ModelAdmin):
+    list_display = ('grades',)
+admin.site.register(RealSchedule, RealScheduleDisplay)
+
+class DraftScheduleDisplay(admin.ModelAdmin):
+    list_display = ('advisorNotes', 'draftRanking')
+    search_fields = ('draftRanking',)
+admin.site.register(DraftSchedule, DraftScheduleDisplay)
+
+#display all courses
+class CourseDisplay(admin.ModelAdmin):
+    list_display = ('title', 'crn', 'creditHours', 'description')
+    search_fields = ('crn', 'title', 'creditHours')
+admin.site.register(Course, CourseDisplay)
--- courseRegistration/models.py
+++ courseRegistration/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+#Table for Schedule
+class Schedule(models.Model):
+    user = models.ForeignKey(User,on_delete=models.DO_NOTHING, related_name='schedule')
+    name = models.CharField(max_length=50)
+    semester = models.CharField(max_length = 6)
+    year = models.IntegerField(default=2023)
+    #courses could be saved as an array of the crn numbers
+    courses = ArrayField(ArrayField(models.CharField(max_length=20)))
+
+    def __str__(self):
+        return self.name
+
+#Table for Course
+class Course(models.Model):
+    title = models.CharField(max_length=20)
+    crn = models.CharField(max_length=20)
+    creditHours = models.IntegerField(default = 0)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+    
+#Table for Real Schedule
+class RealSchedule(models.Model):
+    schedule = models.ForeignKey(Schedule,on_delete=models.CASCADE,related_name='real_schedule')
+    grades = ArrayField(ArrayField(models.DecimalField(max_digits=5, decimal_places=2, default=0)))
+
+    def __str__(self):
+        return self.schedule.name
+    
+#Table for Draft Schedules
+class DraftSchedule(models.Model):
+    schedule = models.ForeignKey(Schedule,on_delete=models.CASCADE,related_name='draft_schedule')
+    advisorNotes = models.CharField(max_length=100)
+    draftRanking = models.IntegerField(default = 1)
+
+    def __str__(self):
+        return self.schedule.name
