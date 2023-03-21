4f61b6f9d69c913172067b178dee0d0b487746d1
lDavis956 <116458122+lDavis956@users.noreply.github.com>
2023-03-21 15:15:56 -0500

    Created tables for tasks and categories

--- taskManager/admin.py
+++ taskManager/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class CategoryDisplay(admin.ModelAdmin):
+    list_display = ('user', 'name')
+    search_fields = ('user', 'name')
+admin.site.register(Category, CategoryDisplay)
+
+class TaskDisplay(admin.ModelAdmin):
+    list_display = ('user', 'category', 'title', 'content', 'startDate', 'endDate', 'reminderDate', 'notificationsOn')
+    search_fields = ('user', 'category', 'title', 'startDate', 'endDate', 'notificationsOn')
+admin.site.register(Task, TaskDisplay)
--- taskManager/models.py
+++ taskManager/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+#Table for Categories
+class Category(models.Model):
+    user = models.ForeignKey(User,on_delete=models.DO_NOTHING, related_name='user_category')
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+#Table for tasks
+class Task(models.Model):
+    user = models.ForeignKey(User,on_delete=models.DO_NOTHING, related_name='user_task')
+    category = models.ForeignKey(Category,on_delete=models.DO_NOTHING,related_name='task_category')
+    title = models.CharField(max_length=20)
+    content = models.CharField(max_length=100)
+    startDate = models.DateTimeField(default= date.today)
+    endDate = models.DateTimeField(default= date.today)
+    reminderDate = models.DateTimeField(default = date.today)
+    notificationsOn = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
