de1bac5c15a0b8f62073042c9fed49e0a5209438
Michaela Snyder <michaela.snyder023@topper.wku.edu>
2023-03-23 13:51:54 -0500

    fixed home.html to incorporate base.html, fixed template directories, added task manager and course registration to base.html navbar

--- PALAPP/settings.py
+++ PALAPP/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'PALAPP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'courseRegistration/templates'),
+                 os.path.join(BASE_DIR, 'taskManager/templates'),
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,12 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-#STATIC_ROOT = 'static/'
-STATIC_ROOT =(BASE_DIR, 'static_collected')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'staticfiles/'),
-# ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
